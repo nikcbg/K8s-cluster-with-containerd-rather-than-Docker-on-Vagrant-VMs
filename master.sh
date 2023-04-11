7e8e566084f343865120c03bdc0f7410262804b6
nikcbg <nikcbg@yahoo.com>
2023-04-11 15:27:10 +0300

    Create master.sh

---
+++ master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install only on master VM and copy the kubeadm join command at the end execute on the worker VM
+echo "[TASK 1] Pull required containers"
+kubeadm config images pull >/dev/null 2>&1
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
+kubeadm init --apiserver-advertise-address=192.168.56.10 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+
+echo "[TASK 3] Deploy Calico network"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+mkdir /home/vagrant/.kube
+cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown -R vagrant:vagrant /home/vagrant/.kube
+
+echo "[TASK 4] Generate and save cluster join command"
+kubeadm token create --print-join-command 
